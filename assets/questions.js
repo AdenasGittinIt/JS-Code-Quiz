06a241175c061838c81933a05b50e419183e422a
Adena McGibbon <justadena@gmail.com>
2019-12-28 12:56:12 -0700

    app 'knows' which question is clicked

--- assets/questions.js
+++ assets/questions.js
@@ -2,13 +2,13 @@ var questions = [
   {
     title: "First Question?",
     multiChoice: ["Answer 1a", "Answer 1b", "Answer 1c", "Answer 1d"],
-    answer: "Answer 1c"
+    answer: "Answer 1a"
   },
 
   {
     title: "Second Question?",
     multiChoice: ["Answer 2a", "Answer 2b", "Answer 2c", "Answer 2d"],
-    answer: "Answer 2c"
+    answer: "Answer 2b"
   },
 
   {
@@ -20,7 +20,7 @@ var questions = [
   {
     title: "Fourth Question?",
     multiChoice: ["Answer 4a", "Answer 4b", "Answer 4c", "Answer 4d"],
-    answer: "Answer 4c"
+    answer: "Answer 4d"
   },
 
 ];
